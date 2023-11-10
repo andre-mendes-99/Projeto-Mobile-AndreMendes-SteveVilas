1cd893604b0a96cc150df20bb39be315dcf8455d
Trasmiter <115027757+Trasmiter@users.noreply.github.com>
2023-11-10 13:25:27 +0000

    Main page beginning

---
+++ Clicker_MobileApp/app/src/main/java/pt/iade/andresteve/clicker/Game_Home_Activity.java
@@ -0,0 +1,14 @@
+package pt.iade.andresteve.clicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Game_Home_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_home);
+    }
+}
