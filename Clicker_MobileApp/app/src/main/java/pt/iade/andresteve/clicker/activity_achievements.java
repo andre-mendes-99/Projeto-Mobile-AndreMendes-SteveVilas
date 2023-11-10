4b9a049d295ecb6c114e3256146a83cf8fbdfd3d
Trasmiter <115027757+Trasmiter@users.noreply.github.com>
2023-11-10 15:46:22 +0000

    Achievements beta

---
+++ Clicker_MobileApp/app/src/main/java/pt/iade/andresteve/clicker/activity_achievements.java
@@ -0,0 +1,14 @@
+package pt.iade.andresteve.clicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_achievements extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achievements);
+    }
+}
