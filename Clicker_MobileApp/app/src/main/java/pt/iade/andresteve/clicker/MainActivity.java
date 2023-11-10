118917dcb1ecc189ec7027e4383ed4f46236fa4d
andre-mendes-99 <andrezmendes18@gmail.com>
2023-11-10 15:49:42 +0000

    add check textboxes

--- Clicker_MobileApp/app/src/main/java/pt/iade/andresteve/clicker/MainActivity.java
+++ Clicker_MobileApp/app/src/main/java/pt/iade/andresteve/clicker/MainActivity.java
@@ -6,11 +6,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button btnregister;
-
+    private EditText textusername;
+    private EditText textpassword;
+    public boolean CheckEntrys(View view)
+    {
+         textusername = (EditText)findViewById(R.id.username_login_text);
+         textpassword =(EditText)findViewById(R.id.password_login_text);
+         if(textpassword.getText().toString().equals("") || textusername.getText().toString().equals(""))
+         {
+             return false;
+         }
+         return true;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +33,9 @@ public class MainActivity extends AppCompatActivity {
         btnregister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(true)
+                if(!CheckEntrys(view))
                 {
-
+                    Toast.makeText(getApplicationContext(), "Preencha as informações que faltam", Toast.LENGTH_LONG).show();
                 }
                 else
                 {
