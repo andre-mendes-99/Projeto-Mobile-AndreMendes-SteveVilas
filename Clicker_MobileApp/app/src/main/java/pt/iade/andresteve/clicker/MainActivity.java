7562c3a7e25d696a5ea93e8f53450e14c0dd5101
andre-mendes-99 <andrezmendes18@gmail.com>
2023-11-14 18:04:03 +0000

    check text boxes and some page redirecting

--- Clicker_MobileApp/app/src/main/java/pt/iade/andresteve/clicker/MainActivity.java
+++ Clicker_MobileApp/app/src/main/java/pt/iade/andresteve/clicker/MainActivity.java
@@ -11,14 +11,15 @@ import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnregister;
-    private EditText textusername;
-    private EditText textpassword;
+    private Button btnRegister;
+    private Button btnLogin;
+    private EditText textUsername;
+    private EditText textPassword;
     public boolean CheckEntrys(View view)
     {
-         textusername = (EditText)findViewById(R.id.username_login_text);
-         textpassword =(EditText)findViewById(R.id.password_login_text);
-         if(textpassword.getText().toString().equals("") || textusername.getText().toString().equals(""))
+         textUsername = (EditText)findViewById(R.id.username_login_text);
+         textPassword =(EditText)findViewById(R.id.password_login_text);
+         if(textPassword.getText().toString().equals("") || textUsername.getText().toString().equals(""))
          {
              return false;
          }
@@ -29,17 +30,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnregister = (Button)findViewById(R.id.main_register_button);
-        btnregister.setOnClickListener(new View.OnClickListener() {
+        btnRegister = (Button)findViewById(R.id.main_register_button);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(!CheckEntrys(view))
                 {
-                    Toast.makeText(getApplicationContext(), "Preencha as informações que faltam", Toast.LENGTH_LONG).show();
+                    //test
+                    Toast.makeText(getApplicationContext(), "Preencha as informações que faltam!", Toast.LENGTH_LONG).show();
                 }
                 else
                 {
                     startActivity(new Intent(MainActivity.this, Register_Activity.class));
+                }
+            }
+
+        });
+
+        btnLogin = (Button)findViewById(R.id.main_submit_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!CheckEntrys(view))
+                {
+                    //test
+                    Toast.makeText(getApplicationContext(), "Preencha as informações que faltam!", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    startActivity(new Intent(MainActivity.this, Game_Home_Activity.class));
                 }
             }
 
--- Clicker_MobileApp/app/src/main/java/pt/iade/andresteve/clicker/Register_Activity.java
+++ Clicker_MobileApp/app/src/main/java/pt/iade/andresteve/clicker/Register_Activity.java
@@ -2,13 +2,64 @@ package pt.iade.andresteve.clicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Register_Activity extends AppCompatActivity {
+    private ImageButton btnGoBack;
+    private Button btnRegister;
+    private EditText textUsername;
+    private EditText textPassword;
+    private EditText textConfirmPass;
+    private EditText textEmail;
+    public boolean TextBoxesFilled(View view)
+    {
+        textUsername = (EditText)findViewById(R.id.username_login_text);
+        textPassword =(EditText)findViewById(R.id.password_login_text);
+        textConfirmPass = (EditText)findViewById(R.id.username_login_text);
+        textEmail =(EditText)findViewById(R.id.password_login_text);
+        if(textPassword.getText().toString().equals("") || textUsername.getText().toString().equals("") || textConfirmPass.getText().toString().equals("") || textEmail.getText().toString().equals(""))
+        {
+            return false;
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        btnGoBack = (ImageButton)findViewById(R.id.go_back_regiter_button);
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    startActivity(new Intent(Register_Activity.this, MainActivity.class));
+            }
+
+        });
+
+
+        btnRegister = (Button)findViewById(R.id.submit_regist_button);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!TextBoxesFilled(view))
+                {
+                    //test
+                    Toast.makeText(getApplicationContext(), "Preencha as informações que faltam!", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    startActivity(new Intent(Register_Activity.this, MainActivity.class));
+                }
+            }
+
+        });
     }
 }
