85d980896a03d630fc6ce51641004e0090fefff3
André de Oliveira Gomes Zépico Mendes <andrezmendes18@gmail.com>
2023-11-14 18:28:58 +0000

    score first try(To test)

--- Clicker_MobileApp/app/src/main/java/pt/iade/andresteve/clicker/Game_Home_Activity.java
+++ Clicker_MobileApp/app/src/main/java/pt/iade/andresteve/clicker/Game_Home_Activity.java
@@ -3,12 +3,30 @@ package pt.iade.andresteve.clicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class Game_Home_Activity extends AppCompatActivity {
-
+    private ImageButton btnClickerTemp;
+    private TextView scoreView;
+    private int score = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_home);
+
+        scoreView = (TextView)findViewById(R.id.score_game_home_textview);
+        scoreView.setText(score);
+        btnClickerTemp = (ImageButton) findViewById(R.id.button_clicker_home_page);
+        btnClickerTemp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             score+=1;
+             scoreView.setText(score);
+            }
+
+        });
     }
 }
