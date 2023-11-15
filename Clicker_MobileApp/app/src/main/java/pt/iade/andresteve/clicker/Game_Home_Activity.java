fcb48a94fff004d5979e78a636da1a1961c7c3d1
andre-mendes-99 <andrezmendes18@gmail.com>
2023-11-15 17:59:15 +0000

    Error correction

--- Clicker_MobileApp/app/src/main/java/pt/iade/andresteve/clicker/Game_Home_Activity.java
+++ Clicker_MobileApp/app/src/main/java/pt/iade/andresteve/clicker/Game_Home_Activity.java
@@ -5,14 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.*;
 import android.widget.TextView;
 
 public class Game_Home_Activity extends AppCompatActivity {
     private ImageButton btnClickerTemp;
+    private Button btnShop;
+    private Button btnAchievments;
+    private Button btnStats;
     private ImageButton btnConfig;
-    private ImageButton btnMinigame;
+    private ImageButton btnMiniGame;
     private TextView scoreView;
     private int score = 0; //temporário deopois começa com o valor da base de dados
     @Override
@@ -20,13 +22,17 @@ public class Game_Home_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_home);
 
+        //Score View:
         scoreView = (TextView)findViewById(R.id.score_game_home_textview);
         scoreView.setText(Integer.toString(score));
 
-        //Buttons
-        btnMinigame= (ImageButton)findViewById(R.id.btn_minigame_game_home);
+        //Buttons:
+        btnMiniGame= (ImageButton) findViewById(R.id.btn_minigame_game_home);
         btnConfig = (ImageButton) findViewById(R.id.button_settings_game_home);
         btnClickerTemp = (ImageButton) findViewById(R.id.button_clicker_home_page);
+        btnShop = (Button)findViewById(R.id.button_shop_game_home);
+        btnAchievments = (Button)findViewById(R.id.button_achievements_game_home);
+        btnStats = (Button)findViewById(R.id.button_stats_game_home);
 
         //botão do clicker:
         btnClickerTemp.setOnClickListener(new View.OnClickListener() {
@@ -40,18 +46,43 @@ public class Game_Home_Activity extends AppCompatActivity {
 
         //botão de configurações:
         btnConfig.setOnClickListener(new View.OnClickListener() {
-            @Override
+            @Override 
             public void onClick(View view) {
                 startActivity(new Intent(Game_Home_Activity.this, Configurations_Activity.class));
             }
         });
 
         //botão minigame:
-        btnMinigame.setOnClickListener(new View.OnClickListener() {
+        btnMiniGame.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
 
                 startActivity(new Intent(Game_Home_Activity.this, Rock_Paper_Scissors_Activity.class));
+            }
+        });
+
+        //botão para loja:
+        btnShop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Game_Home_Activity.this, activity_shop.class));
+            }
+        });
+
+        //botão para achievments:
+        btnAchievments.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Game_Home_Activity.this, activity_achievements.class));
+
+            }
+        });
+
+        //botão para Stats:
+        btnStats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Game_Home_Activity.this, activity_stats.class));
             }
         });
     }
