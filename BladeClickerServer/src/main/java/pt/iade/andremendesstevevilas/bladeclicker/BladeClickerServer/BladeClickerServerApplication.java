29ee09c2a15fbd894c57e069216e7aadf283a7ce
andre-mendes-99 <andrezmendes18@gmail.com>
2023-12-21 23:42:22 +0000

    Inicio do Server

---
+++ BladeClickerServer/src/main/java/pt/iade/andremendesstevevilas/bladeclicker/BladeClickerServer/BladeClickerServerApplication.java
@@ -0,0 +1,13 @@
+package pt.iade.andremendesstevevilas.bladeclicker.BladeClickerServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BladeClickerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BladeClickerServerApplication.class, args);
+	}
+
+}
